1f3787051e8449ce7140513c012052d0f4fa4d97
Tiffany Brown <tiffany@webinista.com>
2017-06-04 12:34:19 -0700

    Fix button unit test since markup changed.

--- test/components/Button.unit.test.jsx
+++ test/components/Button.unit.test.jsx
@@ -13,24 +13,25 @@ const props = {
 
 describe('<Button />', () => {
   const tree = SkinDeep.shallowRender(<Button {...props} />);
+  const button = tree.subTree('button');
 
   it('should be a button', () => {
-    expect(tree.type).to.equal('button');
+    expect(button.type).to.equal('button');
   });
 
   it(`its className should contain ${props.cssClass}`, () => {
-    expect(tree.props.className).to.include(props.cssClass);
+    expect(button.props.className).to.include(props.cssClass);
   });
 
   it(`its text should read "${props.text}"`, () => {
-    expect(tree.text()).to.equal(props.text);
+    expect(button.text()).to.equal(props.text);
   });
 
   it(`its type should be "${props.type}"`, () => {
-    expect(tree.props.type).to.equal(props.type);
+    expect(button.props.type).to.equal(props.type);
   });
 
   it(`its id should be "${props.id}"`, () => {
-    expect(tree.props.id).to.equal(props.id);
+    expect(button.props.id).to.equal(props.id);
   });
 });
