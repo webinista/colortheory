20f8204e81329d5f328df8971474b16572fd114d
Tiffany Brown <tiffany@webinista.com>
2018-01-21 15:37:35 -0800

    Adding webpack.common.js. Closes #85
    - Simplified config/webpack.development.js config/webpack.prod.js
    - Adds webpack-merge module

--- config/webpack.development.js
+++ config/webpack.development.js
@@ -1,45 +1,13 @@
-const path = require('path');  // Include path module from node.js
 const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-const entryConf = [
-  'clipboard',
-  'babel-polyfill',
-  'react',
-  'react-dom',
-  'redux',
-  'react-redux',
-  './src/',
-];
-
-const config = {
-  devtool: 'source-map',
-  entry: entryConf,
-  output: {
-    path: path.resolve(__dirname, '../script'),
-    filename: 'js.js',
-    publicPath: 'script/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          cacheDirectory: '.babelcache',
-        }
-      }
-    ]
-  },
+const devConfig = { 
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
   ],
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  target: 'web'
 };
 
-module.exports = config;
+module.exports = merge(common, devConfig);
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,53 +1,17 @@
-const path = require('path');  // Include path module from node.js
 const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-const entryConf = [
-  'clipboard',
-  'babel-polyfill',
-  'react',
-  'react-dom',
-  'redux',
-  'react-redux',
-  './src/',
-];
-
-const config = {
-  devtool: 'source-map',
-  entry: entryConf,
-  output: {
-    path: path.resolve(__dirname, '../script'),
-    filename: 'js.js',
-    publicPath: 'script/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          cacheDirectory: '.babelcache',
-        }
-      }
-    ]
-  },
+const prodConfig = {
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
-      // TODO: Figure out why this doesn't work
-      // This does not match the documentation, but
-      // using way from documentation causes errors.
-      // See https://webpack.js.org/guides/production-build/#the-manual-way
       sourceMap: true,
       comments: false,
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
     }),
-  ],
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  target: 'web'
+  ]
 };
 
-module.exports = config;
+module.exports = merge(common, prodConfig);
