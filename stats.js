3326a3cc4f9328b93c866cfaf92b1e52085a2b7f
Tiffany Brown <tiffany@webinista.com>
2018-01-21 09:09:58 -0800

    Deleting stats file. Don't need.

--- stats.js
+++
@@ -1,9 +0,0 @@
-const idSite = 1;
-const piwikTrackingApiUrl = 'https://analytics.webinista.com/data/piwik.php';
-
-let _paq = _paq || [];
-_paq.push(['setDocumentTitle', document.title]);
-_paq.push(['setTrackerUrl', piwikTrackingApiUrl]);
-_paq.push(['setSiteId', idSite]);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
