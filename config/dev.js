84e6a467fc2c930ea3f28f877c0b69c2d6cd18aa
Tiffany Brown <tiffany@webinista.com>
2022-01-13 19:37:55 -0800

    Make webpack recognize the .jsx extension.

--- config/dev.js
+++ config/dev.js
@@ -1,19 +1,19 @@
-const webpack   = require('webpack');
+const webpack = require('webpack');
 const { merge } = require('webpack-merge');
-const common    = require('./common.js');
+const common = require('./common');
 
 const devRules = [
   {
     test: /\.css$/,
-    use: [ 'style-loader', 'css-loader' ]
+    use: ['style-loader', 'css-loader']
   }
 ];
 
-const devConfig = { 
-  mode: "development",
+const devConfig = {
+  mode: 'development',
   devtool: 'source-map',
   module: {
-    rules: common.module.rules.concat( devRules )
+    rules: common.module.rules.concat(devRules)
   },
 };
 
