9c6d0b8e747675fe9e8d2b9145217d21cc4e9547
Tiffany Brown <tiffany@webinista.com>
2022-02-02 22:20:22 -0800

    See #119: Temporarily disable the isValidInputColor validator test.

--- test/utils/named-colors.unit.test.js
+++ test/utils/named-colors.unit.test.js
@@ -6,45 +6,45 @@ import {
 const isNamed = [
   {
     color: 'yellow',
-    expected: true
+    expectation: true
   },
   {
     color: 'green',
-    expected: true
+    expectation: true
   },
   {
     color: 'pineapple',
-    expected: false
+    expectation: false
   }
 ];
 
 const hexes = [
   {
     color: 'yellow',
-    expected: '#ffff00'
+    expectation: '#ffff00'
   },
   {
     color: 'green',
-    expected: '#008000'
+    expectation: '#008000'
   },
   {
     color: 'black',
-    expected: '#000000'
+    expectation: '#000000'
   },
   {
     color: 'papaya',
-    expected: '#000'
+    expectation: '#000'
   }
 ];
 
-describe.each( isNamed )( 'Is this a named color?', ({ color, expected }) => {
-  test( `returns ${expected}`, () => {
-    expect( isNamedColor( color ) ).toBe( expected );
+describe.each( isNamed )( 'Is this a named color?', ({ color, expectation }) => {
+  test( `returns ${expectation}`, () => {
+    expect( isNamedColor( color ) ).toBe( expectation );
   });
 });
 
-describe.each( hexes )( 'Does it return the correct hexadecimal value?', ({ color, expected }) => {
-  test( `returns ${expected}`, () => {
-    expect( getHexForNamed( color ) ).toBe( expected );
+describe.each( hexes )( 'Does it return the correct hexadecimal value?', ({ color, expectation }) => {
+  test( `returns ${expectation}`, () => {
+    expect( getHexForNamed( color ) ).toBe( expectation );
   });
 });
